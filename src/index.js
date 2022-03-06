70d328bb62770fbecbc227fbf7a409c2e75907a5
JithuVS <sreejithsalgunan@gmail.com>
2022-03-06 19:27:06 +0530

    setting up socketio

--- src/index.js
+++ src/index.js
@@ -1,15 +1,21 @@
-
-const path = require('path');
-const express = require('express');
+const path = require("path");
+const http = require("http");
+const express = require("express");
+const socketio = require("socket.io");
 
 const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
 
 const port = process.env.PORT || 3000;
-const publicDirectoryPath = path.join(__dirname, '../public'); 
+const publicDirectoryPath = path.join(__dirname, "../public");
 
 app.use(express.static(publicDirectoryPath));
 
+io.on("connection", () => {
+  console.log("New websocket connections");
+});
 
-app.listen(port, ()=>{
-    console.log("server up!");
+server.listen(port, () => {
+  console.log("server up!");
 });
