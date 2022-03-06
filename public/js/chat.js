427fa0175e17979e976dc79b357948e26980a32a
JithuVS <sreejithsalgunan@gmail.com>
2022-03-06 21:54:45 +0530

    basic chat setup

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,1 +1,14 @@
-io()
+const socket = io();
+
+socket.on("message", (message) => {
+  console.log(message);
+});
+
+document.getElementById('messageform').addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  const message = e.target.elements.message.value;
+
+  socket.emit("sendMessage", message);
+});
+
